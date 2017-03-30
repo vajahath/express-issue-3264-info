0091f80ceec370896db7591681a43fabe7c5f903
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-03-30 11:50:56 +0530

    removes an unwanted file

--- serve.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var app = express()
-
-app.post('/', function(req, res) {
-	console.log(req.body.hasOwnProperty('fasdf'));
-	res.send('Hello World')
-})
-
-app.listen(3000)
